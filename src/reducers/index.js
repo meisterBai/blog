878b5c417c8907efc5111d002f4054e1c1c535f8
alan <pianosinglove@163.com>
2019-09-30 13:03:27 +0800

    first commit

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import React, {Component, PropTypes} from 'react'
+import {
+    Redirect
+} from 'react-router-dom'
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
+}
